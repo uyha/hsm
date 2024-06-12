809acf0ec77f8e5a685161cbf011d2a563126395
Uy Ha <hchanuy@gmail.com>
2024-06-12 17:31:10 +0000

    add "Traffic Light" example

--- src/main.zig
+++ src/main.zig
@@ -4,58 +4,49 @@ const hsm = @import("root.zig");
 const state = hsm.state;
 const initial = hsm.initial;
 
-pub fn always() bool {
-    return true;
-}
-pub fn never() bool {
-    return false;
-}
-pub fn sometime(event: Event) bool {
-    return event.Source % 2 == 0;
-}
-
-pub fn hello(event: Event) void {
-    std.debug.print("{s}:{} ({s}) ", .{ @src().file, @src().line, @src().fn_name });
-    std.debug.print("{}\n", .{event});
-}
-pub fn hell(event: Event1, counter: *u8) void {
-    std.debug.print("{s}:{} ({s})\n", .{ @src().file, @src().line, @src().fn_name });
-    counter.* += event.Source;
-}
-pub fn hello1() void {
-    std.debug.print("{s}:{} ({s})\n", .{ @src().file, @src().line, @src().fn_name });
-}
-pub fn hello2() void {
-    std.debug.print("{s}:{} ({s})\n", .{ @src().file, @src().line, @src().fn_name });
-}
-
-const Event = struct {
-    Source: usize,
-};
-const Event1 = struct {
-    Source: u8,
-};
-
 pub fn main() !void {
-    var counter: u8 = 0;
+    @"Traffic light"();
+}
+
+fn harshStop(count: *usize) void {
+    std.debug.print("Harsh stop\n", .{});
+    count.* += 1;
+}
+fn softStop(count: *usize) void {
+    std.debug.print("Soft stop\n", .{});
+    count.* += 1;
+}
+fn slowingDown() void {
+    std.debug.print("Slowing Down\n", .{});
+}
+fn starting() void {
+    std.debug.print("Starting to run\n", .{});
+}
+
+fn @"Traffic light"() void {
+    const Red = struct {};
+    const Yellow = struct {};
+    const Green = struct {};
+
+    const Running = struct {};
+    const Slowing = struct {};
+    const Stopped = struct {};
+
+    var stopCount: usize = 0;
     var sm = hsm.stateMachine(.{
-        .{
-            .initial = true,
-            .src = u8,
-            .event = Event,
-            .actions = .{hello},
-        },
-        .{ .src = u8, .event = Event1, .actions = .{hell} },
-    }, .{&counter});
+        .{ .initial = true, .src = Running, .event = Red, .dst = Stopped, .actions = .{harshStop} },
+        .{ .src = Running, .event = Yellow, .dst = Slowing, .actions = .{slowingDown} },
 
-    sm.process(Event1{ .Source = 1 });
-    sm.process(Event{ .Source = 1 });
-    sm.process(Event1{ .Source = 1 });
-    sm.process(Event{ .Source = 1 });
+        .{ .src = Slowing, .event = Red, .dst = Stopped, .actions = .{softStop} },
 
-    std.debug.print("{}\n", .{counter});
-    //
-    // for (0..10) |i| {
-    //     sm.process(Event{ .Source = @intCast(i) });
-    // }
+        .{ .src = Stopped, .event = Green, .dst = Running, .actions = .{starting} },
+    }, .{&stopCount});
+
+    sm.process(Red{});
+    sm.process(Green{});
+    sm.process(Yellow{});
+    sm.process(Red{});
+    sm.process(Green{});
+
+    std.debug.print("Stops: {}\n", .{stopCount});
 }
