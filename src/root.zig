d2b32df38b6cce79fd96187c92ce2b4f046c336b
Uy Ha <hchanuy@gmail.com>
2025-05-26 06:29:22 +0000

    add more events

--- src/root.zig
+++ src/root.zig
@@ -1,7 +1,17 @@
 const type_list = @import("type_list.zig");
 pub const TypeList = type_list.TypeList;
 
+/// This can be a source state or an event
 pub const Any = struct {};
+
+/// This can be an event
+pub const Entering = struct {};
+/// This can be an event
+pub const Entered = struct {};
+/// This can be an event
+pub const Exiting = struct {};
+/// This can be an event
+pub const Exited = struct {};
 
 const state = @import("state.zig");
 pub const StateMachine = state.StateMachine;
