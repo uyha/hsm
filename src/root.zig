d17936ae479cdfb3338923c7aebcabf661a0b448
Uy Ha <hchanuy@gmail.com>
2025-06-09 16:42:28 +0000

    WIP

--- src/root.zig
+++ src/root.zig
@@ -16,13 +16,13 @@ pub const Exited = struct {};
 const state = @import("state.zig");
 pub const StateMachine = state.StateMachine;
 
-const transitions = @import("transitions.zig");
-pub const Transitions = transitions.Transitions;
+// const transitions = @import("transitions.zig");
+// pub const Transitions = transitions.Transitions;
 
 comptime {
     const testing = @import("std").testing;
 
     testing.refAllDecls(type_list);
     testing.refAllDecls(state);
-    testing.refAllDecls(transitions);
+    // testing.refAllDecls(transitions);
 }
--- src/transitions.zig
+++ src/transitions.zig
@@ -1,3 +1,15 @@
+//  {
+//    state: {
+//      event: [
+//        {
+//          guards: [predicate],
+//          actions: [action],
+//          destination: type
+//        }
+//      ]
+//    }
+//  }
+
 pub fn Transitions(transitions: anytype) type {
     return struct {
         pub fn initCount() usize {
@@ -33,6 +45,46 @@ pub fn Transitions(transitions: anytype) type {
 
                 break :result result;
             };
+        }
+
+        pub fn transitionsFromSource(source: type) type {
+            return comptime result: {
+                var events: type = TypeList(.{});
+                // Guard action destination
+                var gads: type = TypeList(.{});
+
+                for (transitions) |tran| {
+                    if (tran.src != source) {
+                        continue;
+                    }
+
+                    if (events.index(tran.event)) {}
+                }
+            };
+        }
+    };
+}
+
+fn State(source: type, event: type) type {
+    return struct {};
+}
+
+/// Guards actions and destination
+fn Gad(guards_: anytype, actions_: anytype, destination_: type) type {
+    return struct {
+        pub const guards = guards_;
+        pub const actions = actions_;
+        pub const destination = destination_;
+    };
+}
+
+fn Branch(events_: anytype, gads_: type) type {
+    return struct {
+        const events: type = TypeList(events_);
+        const gads: type = TypeList(gads_);
+
+        fn append(event: type, gad: type) type {
+            if (events.index(event)) |index| {}
         }
     };
 }
